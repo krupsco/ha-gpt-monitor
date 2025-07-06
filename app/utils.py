77f093e6202f04b7eeadbac7020f0cdd13b758ca
Mikołaj Krupa <56743697+krupsco@users.noreply.github.com>
2025-07-06 22:57:03 +0200

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,12 @@
+import csv
+import os
+
+def log_data(state, timestamp):
+    filepath = "data/history.csv"
+    write_header = not os.path.exists(filepath)
+    with open(filepath, mode="a", newline="") as file:
+        writer = csv.writer(file)
+        if write_header:
+            writer.writerow(["timestamp", "state"])
+        writer.writerow([timestamp, state])
 
